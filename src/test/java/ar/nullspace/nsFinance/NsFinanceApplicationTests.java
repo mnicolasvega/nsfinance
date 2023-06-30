4ac8f3cb0a3423915c9bd9268a492be99e0339b3
Franco Zanardi <francozanardi.97@gmail.com>
2023-06-30 20:16:08 -0300

    Initializing Spring Boot files

---
+++ src/test/java/ar/nullspace/nsFinance/NsFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.nullspace.nsFinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NsFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
